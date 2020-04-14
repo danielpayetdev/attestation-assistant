88cdceea01669aee6d05687dc7f2fc81f87c349f
Daniel Payet <danieldu974@gmail.com>
2020-04-14 19:48:44 +0200

    fix token undefined

--- functions/src/compte/compte.ts
+++ functions/src/compte/compte.ts
@@ -1,12 +1,12 @@
-import uuidAPIKey from 'uuid-apikey';
+import { v4 as uuid } from 'uuid';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 export const generateApiKey = functions.https.onCall(async (data, context) => {
     const id = context.auth?.uid as string;
-    const key = uuidAPIKey.create().apiKey
+    const key = uuid();
     await admin.firestore().collection('apiKeys').doc(id).set({
-        key: uuidAPIKey.create().apiKey
+        key: key
     });
     return key;
 });
